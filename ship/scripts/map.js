095a3a75f0fc729df8736bfe1e90275ccf991902
Augnelli <will.robertson823@gmail.com>
2016-12-14 12:13:37 -0500

    Create map.js
    Upload from local.

---
+++ ship/scripts/map.js
@@ -0,0 +1,15 @@
+var map = {
+	northWest: document.getElementById('mapNorthWest'),
+	north: document.getElementById('mapNorth'),
+	northEast: document.getElementById('mapNorthEast'),
+	east: document.getElementById('mapEast'),
+	central: document.getElementById('mapCentral'),
+	west: document.getElementById('mapWest'),
+	southWest: document.getElementById('mapSouthWest'),
+	south: document.getElementById('mapSouth'),
+	southEast: document.getElementById('mapSouthEast')
+}
+
+var mapIdentifier = function(){
+	
+}
